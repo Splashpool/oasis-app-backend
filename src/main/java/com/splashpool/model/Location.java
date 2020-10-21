927d7d686a2c44d21d757d28d5555ffd5f30d8ab
chiwong08 <chiandwendy@gmail.com>
2020-10-21 15:21:18 +0100

    working GETS for locations with or without locationName being supplied

--- src/main/java/com/splashpool/model/Location.java
+++ src/main/java/com/splashpool/model/Location.java
@@ -2,19 +2,44 @@ package com.splashpool.model;
 
 public class Location {
 
-    private String locationName;
     private long locationId;
+    private String locationName;
+    private String address1;
+    private String address2;
+    private String city;
+    private String postCode;
+    private String country;
+    private Float  longitude;
+    private Float  latitude;
+    private String adminOrg;
 
-    public Location(String locationName, long locationId) {
+    public Location(Long locationId, String locationName,
+                    String address1, String address2, String city, String postCode, String country,
+                    Float longitude, Float latitude, String adminOrg) {
+        this.locationId   = locationId;
         this.locationName = locationName;
-        this.locationId = locationId;
+        this.address1     = address1;
+        this.address2     = address2;
+        this.city         = city;
+        this.postCode     = postCode;
+        this.country      = country;
+        this.longitude    = longitude;
+        this.latitude     = latitude;
+        this.adminOrg     = adminOrg;
     }
 
+    public Long getLocationId() {
+        return locationId;
+    }
     public String getLocationName() {
         return locationName;
     }
-
-    public long getLocationId() {
-        return locationId;
-    }
+    public String getAddress1() { return address1; }
+    public String getAddress2() { return address2; }
+    public String getCity() { return city; }
+    public String getPostCode() { return postCode; }
+    public String getCountry() { return country; }
+    public Float  getLongitude() { return longitude; }
+    public Float  getLatitude() { return latitude; }
+    public String getAdminOrg() { return adminOrg; }
 }
