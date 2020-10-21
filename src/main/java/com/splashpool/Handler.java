dce8e7b94d6943267dc95bda32921a7a631389d0
Eva Coffield <eva@darcoff.com>
2020-10-21 11:48:22 +0100

    Set up env variables and CORS headers

--- src/main/java/com/splashpool/Handler.java
+++ src/main/java/com/splashpool/Handler.java
@@ -7,22 +7,25 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
 	private static final Logger LOG = LogManager.getLogger(Handler.class);
 
-//	private String DB_HOST = System.getenv("DB_HOST");
-//	private String DB_NAME = System.getenv("DB_NAME");
-//	private String DB_USER = System.getenv("DB_USER");
-//	private String DB_PASSWORD = System.getenv("DB_PASSWORD");
+	private String DB_HOST = System.getenv("DB_HOST");
+	private String DB_NAME = System.getenv("DB_NAME");
+	private String DB_USER = System.getenv("DB_USER");
+	private String DB_PASSWORD = System.getenv("DB_PASSWORD");
 
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
 		LOG.info("received: {}", input);
+
+		// CORS from anywhere
+		HashMap<String, String> headers = new HashMap<>();
+		headers.put("Access-Control-Allow-Origin", "*");
+		headers.put("Access-Control-Allow-Headers", "Content-Type");
 
 //		String userId = (String) ((Map)input.get("queryStringParameters")).get("userId");
 
@@ -31,14 +34,13 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 
-			Connection connect = DriverManager.getConnection("jdbc:mysql://oasis.cpgnf1zc8lar.eu-west-2.rds.amazonaws.com/oasis?"
-					+ "user=<aUser>&password=<password>");
+			Connection connect = DriverManager.getConnection(String.format("jdbc:mysql://%s/%s?user=%s&password=%s", DB_HOST, DB_NAME, DB_USER, DB_PASSWORD));
 
 			Statement statement = connect.createStatement();
 			ResultSet resultSet = statement.executeQuery("select locationName, locationId from oasis.Location");
 
 //			 Connection connection = DriverManager.getConnection(String.format(
-//			 		"jdbs:mysql://%s/%s?user=%s&password=%s", DB_HOST, DB_NAME, DB_USER, DB_PASSWORD));
+//			 		"jdbc:mysql://%s/%s?user=%s&password=%s", DB_HOST, DB_NAME, DB_USER, DB_PASSWORD));
 //			 PreparedStatement preparedStatement = connection.prepareStatement(
 //					"SELECT locationName, locationId from Location where LocationName =?");
 //			 preparedStatement.setString(1, userId);
@@ -62,6 +64,7 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(locations)
+				.setHeaders(headers)
 				.build();
 	}
 }
